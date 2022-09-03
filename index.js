7f470e32c2c452fcb3b4f399388b8f782249a773
nilssvensson <nils27school@gmail.com>
2022-09-03 16:47:22 -0400

    commit tree bfff5a3ca657b1fb45adae3eea5cd14f13074319

--- index.js
+++ index.js
@@ -11,6 +11,40 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// "What Does The This Keyword Mean?",
+// "What Is The Constructor OO Pattern?",
+// "Implementing Blockchain Web API",
+// "The Test Driven Development Workflow",
+// "What Is NaN And How Can We Check For It",
+// "What Is The Difference Between StopPropagation And PreventDefault?",
+// "Immutable State And Pure Functions",
+// "What Is The Difference Between == And ===?",
+// "What Is The Difference Between Event Capturing And Bubbling?",
+// "What Is JSONP?"
+
+
+// const titleCased = () => {
+//   return tutorials;
+// }
+// function capitalize(word) {
+//  return word[0].toUpperCase() + word.slice(1)
+// }
+
+// text.split(' ')
+
+// for (let i = 0; i < wordsSplit.length; i++) [
+//   let capitalize(word)
+// ]
+
+// wordsSplit.map(capatalize).join(" ")
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((line) => {
+    const tokens = line.split(" ");
+    const capitalizedTokens = tokens.map(
+      (token) => token.charAt(0).toUpperCase() + token.slice(1)
+    );
+    const response = capitalizedTokens.join(" ");
+    return response;
+  });
+};
